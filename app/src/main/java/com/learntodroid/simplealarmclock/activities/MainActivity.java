c495da4cd078f74de881fc6265d6c3ddae98d118
dungpv100101 <dungpv100101@gmail.com>
2023-03-09 03:55:11 +0700

    navbar

--- app/src/main/java/com/learntodroid/simplealarmclock/activities/MainActivity.java
+++ app/src/main/java/com/learntodroid/simplealarmclock/activities/MainActivity.java
@@ -4,8 +4,15 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.learntodroid.simplealarmclock.R;
+import com.learntodroid.simplealarmclock.alarmslist.AlarmsListFragment;
+import com.learntodroid.simplealarmclock.worldtime.WorldTimeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
